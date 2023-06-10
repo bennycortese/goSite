63fb1f429f6a9006675d5b3b0f2463c092ba85d0
benny <bennycortese@gmail.com>
2023-06-10 10:13:22 -0700

    working golang integration with forms, website scraping functionality

--- static-site/main.go
+++ static-site/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -8,10 +9,30 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
+	http.HandleFunc("/action_page.html", handleFormSubmission)
 
 	log.Print("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+}
+
+func handleFormSubmission(w http.ResponseWriter, r *http.Request) {
+	if true {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Failed to parse form data", http.StatusBadRequest)
+			return
+		}
+
+		website := r.Form.Get("website")
+		resp, err := http.Get(website)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Fprintf(w, "<h1> Website: %v </h1>", resp.Body)
 	}
 }
